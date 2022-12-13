37ad424411370256002c3382eba67ebdaa818197
Saeid <98920239+saeedlk@users.noreply.github.com>
2022-12-13 04:48:59 +0330

    Update app.sh

--- app.sh
+++ app.sh
@@ -6,4 +6,4 @@ sudo yum update -y
 
 sudo yum install -y awscli
 
-sudo DD_API_KEY=1955c975a4b6eb90ba251ac45a80104b DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
+sudo DD_API_KEY=1955c975a4b6eb90ba251ac45a80104b DD_SITE="datadoghq.com" DD_LOGS_ENABLED="true" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
